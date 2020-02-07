828fdf24bed18e8b92289fd8de598ec87260343d
Simon Urbanek <simon.urbanek@R-project.org>
2020-02-07 13:39:39 +1300

    add basic spot request tools and configs

--- spot-request.sh
+++ spot-request.sh
@@ -1,23 +1,32 @@
-if [ -z "$1" -o "x$1" = x-h ]; then
+if [ "x$1" = x-h ]; then
     echo ''
-    echo " Usage: $0 <specs.json> [<volume-id>]"
+    echo " Usage: $0 [<specs.json> [<volume-id>]]"
     echo ''
     echo ' Requests a spot instance according to the JSON specs file'
     echo ' attaches the volume (if provided) and queries the'
     echo ' public IP address.'
     echo ' There is no timeout limit for individual steps so it should'
     echo ' be run interactively.'
+    echo ' The default specs are spot-default.json'
     echo ''
     echo ' All responses are logged to spot-request.log'
     echo ''
     echo ' Env vars: MAXPRICE - max price (defaults to 0.02)'
+    echo '           AMI      - image ot use (overrides the json)'
     echo ''
     exit 1
 fi
 
 FN="$1"
+if [ -z "$FN" ]; then FN=spot-default.json; fi
 if [ -z "$MAXPRICE" ]; then MAXPRICE=0.02; fi
 VOL="$2"
+
+if [ -n "$AMI" ]; then
+    TF="spot.tmp"
+    sed 's/"ImageId":.*/"ImageId": "'$AMI'",/' "$FN" > "$TF"
+    FN="$TF"
+fi
 
 echo " - Requesting instance according to $FN with max price $MAXPRICE"
 sid=`aws ec2 request-spot-instances --spot-price $MAXPRICE --launch-specification file://$FN | tee spot-request.log | sed -n 's/.*"SpotInstanceRequestId": "//p' | sed 's:".*::'`
@@ -60,3 +69,10 @@ fi
 echo -n " - IP address(es): "
 ip=`aws ec2 describe-instances --instance-ids $iid | sed -n 's/"PublicIp": "//p' | tee -a spot-request.log | sed 's:".*::'`
 echo $ip
+
+if aws ec2 describe-instances --instance-ids $iid | grep '"Platform": "windows"' >/dev/null; then
+    echo ' - This is a Windows, to get password:'
+    echo "aws ec2 get-password-data --instance-id $iid --priv-launch-key"
+fi
+echo ''
+
